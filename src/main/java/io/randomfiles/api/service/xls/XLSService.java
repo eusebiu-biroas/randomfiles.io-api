c5673f945d34028da85cfe2321cb4fe044061237
Eusebiu Biroas <eusebiu@biroas.com>
2022-10-28 02:05:46 +0300

    - fix quality gate, use try-with-resources on HSSFWorkbook

--- src/main/java/io/randomfiles/api/service/xls/XLSService.java
+++ src/main/java/io/randomfiles/api/service/xls/XLSService.java
@@ -30,24 +30,24 @@ public class XLSService {
     }
 
     public ByteArrayOutputStream generateXLS() throws IOException {
-
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        Workbook workbook = new HSSFWorkbook();
-        Sheet sheet = workbook.createSheet();
+        try (Workbook workbook = new HSSFWorkbook()) {
+            Sheet sheet = workbook.createSheet();
 
-        for (int rowCount = 0; rowCount < 3; rowCount++) {
-            Row row = sheet.createRow(rowCount);
-            Cell cell = row.createCell(1);
-            cell.setCellValue(randomService.getRandomString());
+            for (int rowCount = 0; rowCount < 3; rowCount++) {
+                Row row = sheet.createRow(rowCount);
+                Cell cell = row.createCell(1);
+                cell.setCellValue(randomService.getRandomString());
 
-            cell = row.createCell(2);
-            cell.setCellValue(config.getWaterMarkText());
+                cell = row.createCell(2);
+                cell.setCellValue(config.getWaterMarkText());
 
-            cell = row.createCell(3);
-            cell.setCellValue(ZonedDateTime.now().format(DateTimeFormatter.ISO_ZONED_DATE_TIME));
+                cell = row.createCell(3);
+                cell.setCellValue(ZonedDateTime.now().format(DateTimeFormatter.ISO_ZONED_DATE_TIME));
+            }
+
+            workbook.write(byteArrayOutputStream);
         }
-
-        workbook.write(byteArrayOutputStream);
         return byteArrayOutputStream;
     }
 
