ab8ccc3c7c3c819b715409b04923e75986aad2c8
cristina313 <cristina.if890@gmail.com>
2022-10-28 20:25:29 +0300

    Generate random xlsx

---
+++ src/test/java/io/randomfiles/api/controller/XLSXControllerTest.java
@@ -0,0 +1,55 @@
+package io.randomfiles.api.controller;
+
+import io.randomfiles.api.RandomFilesApplication;
+import io.randomfiles.api.service.xlsx.XLSXService;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.inject.Inject;
+import java.io.ByteArrayOutputStream;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RandomFilesApplication.class)
+@AutoConfigureMockMvc
+public class XLSXControllerTest {
+    @Inject
+    private MockMvc mockMvc;
+    @MockBean
+    private XLSXService xlsxService;
+
+    @Before
+    public void setUp() throws Exception {
+
+        ByteArrayOutputStream byteArrayOutputStreamStream = new ByteArrayOutputStream();
+        byteArrayOutputStreamStream.write(1);
+
+        Mockito.when(xlsxService.generateXLSX()).thenReturn(byteArrayOutputStreamStream);
+    }
+
+    @Test
+    public void getXLSXTest() throws Exception {
+        String url = "/api/rest/v1/xlsx";
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(url))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        assert (mvcResult.getResponse().getContentLength() > 0);
+        Assert.assertThat(mvcResult.getResponse().getContentType()
+                , CoreMatchers.equalTo(MediaType.APPLICATION_OCTET_STREAM_VALUE));
+
+    }
+}
